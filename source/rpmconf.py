d40055b40e31f88f3a1b006c45de582c98bcd3a6
Miroslav Suchý <miroslav@suchy.cz>
2014-11-18 08:26:16 +0100

    simplify condition

--- source/rpmconf.py
+++ source/rpmconf.py
@@ -60,10 +60,7 @@ def differ(file_name1, file_name2):
     """ returns True if files differ """
     fromlines = open(file_name1, 'U').readlines()
     tolines = open(file_name2, 'U').readlines()
-    if len(list(difflib.unified_diff(fromlines, tolines))) == 0:
-        return False
-    else:
-        return True
+    return not(list(difflib.unified_diff(fromlines, tolines)) == [])
 
 def show_diff(file1, file2):
     fromdate = time.ctime(os.stat(file1).st_mtime)
