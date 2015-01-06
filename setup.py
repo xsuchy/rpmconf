997801d51e82353651f0856eb4c07395b01b400a
Igor Gnatenko <i.gnatenko.brain@gmail.com>
2015-01-06 18:04:58 +0300

    Split to python class and CLI
    Reference: https://github.com/xsuchy/rpmconf/issues/16
    Signed-off-by: Igor Gnatenko <i.gnatenko.brain@gmail.com>

---
+++ setup.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+
+from distutils.core import setup
+
+setup(name = "rpmconf",
+      packages = ["rpmconf"],
+      version = "1.0.90",
+      description = "Handle rpmnew and rpmsave files",
+      author = "Igor Gnatenko",
+      author_email = "i.gnatenko.brain@gmail.com",
+      maintainer = "Miroslav Suchý",
+      maintainer_email = "msuchy@redhat.com",
+      url = "https://github.com/xsuchy/rpmconf",
+      keywords = ["rpm"],
+      scripts = ["bin/rpmconf"]
+     )
