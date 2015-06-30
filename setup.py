b6d5ae5c450cfc9ea28f749d81ae4ffba2c11548
Miroslav Suchý <msuchy@redhat.com>
2015-06-30 14:24:08 +0200

    Automatic commit of package [rpmconf] release [1.0.12-1].

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from distutils.core import setup
 
 setup(name = "rpmconf",
       packages = ["rpmconf"],
-      version = "1.0.11",
+      version = "1.0.12",
       description = "Handle rpmnew and rpmsave files",
       author = "Igor Gnatenko",
       author_email = "i.gnatenko.brain@gmail.com",
